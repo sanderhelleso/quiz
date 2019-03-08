08d6c350ee6512e2f2cf1446be706603a2364bb7
Sanderhelleso <sanderhelleso@hotmail.com>
2019-03-08 00:09:26 -0800

    added shuffle slice and swap

--- lib/quiz.go
+++ lib/quiz.go
@@ -3,20 +3,41 @@ package lib
 import (
 	"bufio"
 	"fmt"
+	"math/rand"
 	"os"
 	"strings"
+	"time"
 )
+
+const message string = `
+////////////////////////////////////////
+// Welcome to the amazing quiz game! //
+//////////////////////////////////////
+`
 
 // Quiz starts the quiz and print out the new question
 // after each guess, accumulating the total score
 // returns the total time, score and percentage correct
 func Quiz(questions *[]Question) {
 
-	// initialize score
-	var score int
+	// print out welcome screen
+	fmt.Println(message + "\n")
 
 	// initialize a new reader
 	reader := bufio.NewReader(os.Stdin)
+
+	// initialize score
+	var score int
+
+	// ask if questions should be shuffeled
+	fmt.Print("Would you like to shuffle the questions? (yes/no): ")
+	shouldShuffle, _ := reader.ReadString('\n')
+	shouldShuffle = trimString(&shouldShuffle)
+
+	// check answer, and shuffle if "yes"
+	if strings.Compare(shouldShuffle, "yes") == 0 {
+		shuffleSlice(questions)
+	}
 
 	// iterate over all the questions and print
 	// each question one by one and wait for answer
@@ -27,7 +48,7 @@ func Quiz(questions *[]Question) {
 		answer, _ := reader.ReadString('\n')
 
 		// trim input correctly to make it comparable
-		answer = strings.ToLower(strings.Trim(answer, " \r\n"))
+		answer = trimString(&answer)
 
 		// compare answer to input
 		if strings.Compare(answer, q.answer) == 0 {
@@ -45,7 +66,38 @@ func Quiz(questions *[]Question) {
 		getPercentage(score, len(*questions)))
 }
 
+func setTimer() int {
+	return 2
+}
+
 // getPercentage returns the percentage of total by max
 func getPercentage(total int, max int) int {
 	return int(float64(total) / float64(max) * 100)
+}
+
+func trimString(input *string) string {
+	return strings.ToLower(strings.Trim(*input, " \r\n"))
+}
+
+// shuffle the passed in slice, modifying the
+// original slice in 0(n)
+func shuffleSlice(q *[]Question) {
+
+	// set the seed for randomnezzz
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	for i := 0; i < len(*q); i++ {
+
+		// get values by random indexes
+		q1 := &(*q)[rand.Int()%len(*q)]
+		q2 := &(*q)[rand.Int()%len(*q)]
+
+		// swap values retrieved by random indexes
+		swap(q1, q2)
+	}
+}
+
+// swap swaps two values passed in by memory referance
+func swap(q1, q2 *Question) {
+	*q1, *q2 = *q2, *q1
 }
